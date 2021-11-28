c43d7610681ef90874b0ae151a3126c389f4f388
bedjil <sbedjil@yahoo.com>
2021-11-28 22:56:40 +0100

    first commit

---
+++ exercises/clippy/target/debug/deps/clippy2-e3d763da57364d6c.d
@@ -0,0 +1,7 @@
+/home/samra/rustlings/exercises/clippy/target/debug/deps/clippy2-e3d763da57364d6c.rmeta: clippy2.rs
+
+/home/samra/rustlings/exercises/clippy/target/debug/deps/clippy2-e3d763da57364d6c.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
