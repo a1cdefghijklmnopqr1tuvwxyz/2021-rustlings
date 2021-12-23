4b00b506cd20da895f9731e6751b3ae711f6ef7d
sbedjil <72738073+sbedjil@users.noreply.github.com>
2021-12-23 22:25:23 +0100

    Delete move_semantics4.rs

--- exercises/move_semantics/move_semantics4.rs
+++
@@ -1,7 +0,0 @@
-// move_semantics4.rs
-// Refactor this code so that instead of having `vec0` and creating the vector
-// in `fn main`, we create it within `fn fill_vec` and transfer the
-// freshly created vector from fill_vec to its caller.
-// Execute `rustlings hint move_semantics4` for hints!
-
-// I AM NOT DONE
