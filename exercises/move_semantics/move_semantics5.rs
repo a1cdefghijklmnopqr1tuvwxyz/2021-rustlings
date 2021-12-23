07d0cdba278004f379578bba0cf3515c28d4562d
sbedjil <72738073+sbedjil@users.noreply.github.com>
2021-12-23 22:25:12 +0100

    Delete move_semantics5.rs

--- exercises/move_semantics/move_semantics5.rs
+++
@@ -1,6 +0,0 @@
-// move_semantics5.rs
-// Make me compile only be reordering the lines in `main()`, but without
-// adding, changing or removing any of them.
-// Execute `rustlings hint move_semantics5` for hints :)
-
-// I AM NOT DONE
